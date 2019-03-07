1e27ce3f434c8af531dfece5e221e0ada9f1817f
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-03-07 00:56:31 -0300

    testes de estilo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
+// counter dash
+
+$(document).ready(function() {
+
+  var counters = $(".count");
+  var countersQuantity = counters.length;
+  var counter = [];
+
+  for (i = 0; i < countersQuantity; i++) {
+    counter[i] = parseInt(counters[i].innerHTML);
+  }
+
+  var count = function(start, value, id) {
+    var localStart = start;
+    setInterval(function() {
+      if (localStart < value) {
+        localStart++;
+        counters[id].innerHTML = localStart;
+      }
+    }, 40);
+  }
+
+  for (j = 0; j < countersQuantity; j++) {
+    count(0, counter[j], j);
+  }
+});
+
+
+
+
 // window.onload = function(){
 //   showIndicadores();
 // }
@@ -317,3 +347,7 @@ fugiat consequuntur perferendis explicabo, amet ipsa ex dolor vitae sed quasi to
         
       </article>`
 }
+
+
+
+
