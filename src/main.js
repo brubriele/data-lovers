ed15c02183f66fcbd619f5b638aefae83ef434cc
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-03-08 12:45:53 -0300

    cria template populacao

--- src/main.js
+++ src/main.js
@@ -8,9 +8,16 @@ document.getElementById('btn-topic-viol').addEventListener('click', () => {
   printValue()
 })
 
+document.getElementById('btn-topic-pop').addEventListener('click', () => {
+  showIndicadoresPop();
+  printValue()
+})
+
+
+
 function printValue() {
 
-  let valor = document.getElementById("formEduca").indicator.value;
+  let valor = document.getElementById("formIndic").indicator.value;
   let basePeru = WORLDBANK.PER.indicators
   let baseBrasil = WORLDBANK.BRA.indicators
   let filtrado = WORLDBANK.BRA.indicators
@@ -99,7 +106,7 @@ function showIndicadoresEduca() {
         </div>
       </div>
 
-      <form id="formEduca" action="">
+      <form id="formIndic" action="">
         <input type="radio" name="indicator" value="Proporción de inscripciones de mujeres con respecto a varones en la educación terciaria (%)"> Proporção de matrículas de mulheres em relação a homens no
         ensino superior (%)<br>
         <input type="radio" name="indicator" value="Proporción de inscripciones de mujeres con respecto a varones en la educación secundaria (%)"> Proporção de matrículas de mulheres em relação ao sexo
@@ -149,7 +156,7 @@ function showIndicadoresViol() {
         </div>
       </div>
 
-      <form id="formEduca" action="">
+      <form id="formIndic" action="">
         <input type="radio" name="indicator" value="Mujeres que creen que está justificado que un marido golpee a su esposa (cualquiera de las cinco razones) (%)"> Mulheres que acreditam que um marido tem justificativa de bater em sua esposa (qualquer uma das cinco razões*) (%)<br>
         <input type="radio" name="indicator" value="Proporción de mujeres víctimas de violencia física o sexual en los últimos 12 meses (% de mujeres de entre 15 y 49 años)"> Proporção de mulheres vítimas de violência física ou sexual nos últimos 12 meses (% de mulheres entre 15 e 49 anos)<br>
         <input id="btnEscolha" type="button" value="Explorar">
@@ -173,3 +180,51 @@ function showIndicadoresViol() {
 `
 }
 
+function showIndicadoresPop() {
+  let indicaDiv = document.getElementById('indicator-selection');
+  indicaDiv.innerHTML = `
+  <div class="box-select">
+      <div class="box-country">
+        <div class="bra">
+          <a class="btn-coun*4try bra" href="#"><img class="img-bra" src="img/bra.png" alt=""></a>
+          <p class="p-country">Brasil</p>
+
+        </div>
+        <div class="mex">
+          <a class="btn-country mex" href="#"><img class="img-mex" src="img/mex.png" alt=""></a>
+          <p class="p-country">México</p>
+        </div>
+
+        <div class="per">
+          <a class="btn-country per" href=""><img class="img-per" src="img/per.png" alt=""></a>
+          <p class="p-country">Perú</p>
+        </div>
+        <div class="chi">
+          <a class="btn-country chi" href=""><img class="img-chi" src="img/chi.png" alt=""></a>
+          <p class="p-country">Chile</p>
+        </div>
+      </div>
+
+      <form id="formIndic" action="">
+        <input type="radio" name="indicator" value="Población activa, mujeres (% de la población activa total)"> População activa, mulheres (% do total da população activa)<br>
+        <input type="radio" name="indicator" value="Población entre 15 y 64 años de edad, mujeres (% del total)"> População entre 15 e 64 anos de idade, mulheres (% do total)<br>
+        <input id="btnEscolha" type="button" value="Explorar">
+      </form>
+
+      <p id="indicators-div-peru"></p>
+      <p id="indicators-div-brasil"></p>
+
+
+    </div>
+    <article>
+      <div class="ball-topic"></div>
+      <h1>VIOLÊNCIA</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam numquam sit facilis odit magnam nulla rem
+        fugiat consequuntur perferendis explicabo, amet ipsa ex dolor vitae sed quasi totam, veritatis nihil?</p>
+      <div class="element">
+          <div class="elemA"></div>
+          <div class="elemB"></div>
+      </div>
+    </article>
+`
+}
