19279897557da0f9286c8cf19cab9bebfc79a295
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-03-11 00:55:51 -0300

    fatora e implementa table em template

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-window.onload = function(){
-  listeners()
-}
+// window.onload = function(){
+//   listeners()
+// }
 
-function listeners(){
-  
 document.getElementById('btn-topic-educa').addEventListener('click', () => {
   showIndicadoresEduca();
   elements = document.getElementById("btnEscolha");
@@ -13,16 +11,18 @@ document.getElementById('btn-topic-educa').addEventListener('click', () => {
 
 document.getElementById('btn-topic-viol').addEventListener('click', () => {
   showIndicadoresViol();
+  elements = document.getElementById("btnEscolha");
+  elements.addEventListener('click', printValue);
   printValue()
 })
 
 document.getElementById('btn-topic-pop').addEventListener('click', () => {
   showIndicadoresPop();
+  elements = document.getElementById("btnEscolha");
+  elements.addEventListener('click', printValue);
   printValue()
   // doPais("BRA")
 })
-
-}
 
 // function doPais(pais) {
 //   WORLDBANK[pais]
@@ -33,23 +33,44 @@ function printValue() {
   let valor = document.getElementById("formIndic").indicator.value;
   let basePeru = WORLDBANK.PER.indicators
   let baseBrasil = WORLDBANK.BRA.indicators
+  let baseMexico = WORLDBANK.MEX.indicators
+  let baseChile = WORLDBANK.CHL.indicators
   let selecionadoPeru = indexIndicator(basePeru, 'indicatorName', valor)
   let selecionadoBrasil = indexIndicator(baseBrasil, 'indicatorName', valor)
+  let selecionadoMexico = indexIndicator(baseMexico, 'indicatorName', valor)
+  let selecionadoChile = indexIndicator(baseChile, 'indicatorName', valor)
   let escolhaPeru = WORLDBANK.PER.indicators[selecionadoPeru]
   let escolhaBrasil = WORLDBANK.BRA.indicators[selecionadoBrasil]
-  console.log(selecionadoPeru);
-  console.log(escolhaPeru.data["2005"])
-  // console.log(base[escolha])
-  //  console.log(typeof escolhaPeru)
+  let escolhaMexico = WORLDBANK.BRA.indicators[selecionadoMexico]
+  let escolhaChile = WORLDBANK.BRA.indicators[selecionadoChile]
+
+  console.log("PErú", escolhaPeru.data["2013"], escolhaPeru.data["2014"], escolhaPeru.data["2015"])
+  console.log(escolhaBrasil.data["2013"], escolhaBrasil.data["2014"], escolhaBrasil.data["2015"])
+
+  arrayBra = [
+    escolhaBrasil.data["2013"],
+    escolhaBrasil.data["2014"],
+    escolhaBrasil.data["2015"]
+  ]
+  document.getElementById('BRA-2013').innerHTML = arrayBra[0]
+  document.getElementById('BRA-2014').innerHTML = arrayBra[1]
+  document.getElementById('BRA-2015').innerHTML = arrayBra[2]
 
 
-  document.getElementById('indicators-div-peru').innerHTML = "Perú 2013:  " + escolhaPeru.data["2013"]
-  document.getElementById('indicators-div-peru').innerHTML = "Perú 2014:  " + escolhaPeru.data["2014"]
-  document.getElementById('indicators-div-peru').innerHTML = "Perú 2015:  " + escolhaPeru.data["2015"]
+  document.getElementById('PER-2013').innerHTML = escolhaPeru.data["2013"]
+  document.getElementById('PER-2014').innerHTML = escolhaPeru.data["2014"]
+  document.getElementById('PER-2015').innerHTML = escolhaPeru.data["2015"]
 
-  document.getElementById('indicators-div-brasil').innerHTML = "Brasil 2013:  " + escolhaBrasil.data["2013"]
-  document.getElementById('indicators-div-brasil').innerHTML ="Brasil 2014:  " + escolhaBrasil.data["2014"]
-  document.getElementById('indicators-div-brasil').innerHTML = "Brasil 2015:  " + escolhaBrasil.data["2015"]
+
+  document.getElementById('MEX-2013').innerHTML = escolhaMexico.data["2013"]
+  document.getElementById('MEX-2014').innerHTML = escolhaMexico.data["2014"]
+  document.getElementById('MEX-2015').innerHTML = escolhaMexico.data["2015"]
+
+
+  document.getElementById('CHL-2013').innerHTML = escolhaChile.data["2013"]
+  document.getElementById('CHL-2014').innerHTML = escolhaChile.data["2014"]
+  document.getElementById('CHL-2015').innerHTML = escolhaChile.data["2015"]
+
 
   // descobrir como se livrar do maldito undefined
   elements = document.getElementById("btnEscolha");
@@ -110,6 +131,39 @@ function showIndicadoresEduca() {
         fugiat consequuntur perferendis explicabo, amet ipsa ex dolor vitae sed quasi totam, veritatis nihil?</p>
         <div id="indicators-div-peru"></div>
         <div id="indicators-div-brasil"></div>
+        
+<table class="rwd-table">
+<tr>
+  <th>País</th>
+  <th>2013</th>
+  <th>2014</th>
+  <th>2015</th>
+</tr>
+<tr>
+  <td data-th="name-BRA">Brasil</td>
+  <td data-th="BRA-2013" id="BRA-2013">2013</td>
+  <td data-th="BRA-2014" id="BRA-2014">2014</td>
+  <td data-th="BRA-2015" id="BRA-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-PER">Perú</td>
+    <td data-th="PER-2013" id="PER-2013">2013</td>
+    <td data-th="PER-2014" id="PER-2014">2014</td>
+    <td data-th="PER-2015" id="PER-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-CHL">Chile</td>
+    <td data-th="CHL-2013" id="CHL-2013">2013</td>
+    <td data-th="CHL-2014" id="CHL-2014">2014</td>
+    <td data-th="CHL-2015" id="CHL-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-MEX">México</td>
+    <td data-th="MEX-2013" id="MEX-2013">2013</td>
+    <td data-th="MEX-2014" id="MEX-2014">2014</td>
+    <td data-th="MEX-2015" id="MEX-2015">2015</td>
+</tr>
+</table>
     </article>
 `
 }
@@ -154,6 +208,39 @@ function showIndicadoresViol() {
         fugiat consequuntur perferendis explicabo, amet ipsa ex dolor vitae sed quasi totam, veritatis nihil?</p>
         <p id="indicators-div-peru"></p>
         <p id="indicators-div-brasil"></p>
+        
+<table class="rwd-table">
+<tr>
+  <th>País</th>
+  <th>2013</th>
+  <th>2014</th>
+  <th>2015</th>
+</tr>
+<tr>
+  <td data-th="name-BRA">Brasil</td>
+  <td data-th="BRA-2013" id="BRA-2013">2013</td>
+  <td data-th="BRA-2014" id="BRA-2014">2014</td>
+  <td data-th="BRA-2015" id="BRA-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-PER">Perú</td>
+    <td data-th="PER-2013" id="PER-2013">2013</td>
+    <td data-th="PER-2014" id="PER-2014">2014</td>
+    <td data-th="PER-2015" id="PER-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-CHL">Chile</td>
+    <td data-th="CHL-2013" id="CHL-2013">2013</td>
+    <td data-th="CHL-2014" id="CHL-2014">2014</td>
+    <td data-th="CHL-2015" id="CHL-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-MEX">México</td>
+    <td data-th="MEX-2013" id="MEX-2013">2013</td>
+    <td data-th="MEX-2014" id="MEX-2014">2014</td>
+    <td data-th="MEX-2015" id="MEX-2015">2015</td>
+</tr>
+</table>
 
     </article>
 `
@@ -201,6 +288,55 @@ function showIndicadoresPop() {
         fugiat consequuntur perferendis explicabo, amet ipsa ex dolor vitae sed quasi totam, veritatis nihil?</p>
         <p id="indicators-div-peru"></p>
       <p id="indicators-div-brasil"></p>
+      
+<table class="rwd-table">
+<tr>
+  <th>País</th>
+  <th>2013</th>
+  <th>2014</th>
+  <th>2015</th>
+</tr>
+<tr>
+  <td data-th="name-BRA">Brasil</td>
+  <td data-th="BRA-2013" id="BRA-2013">2013</td>
+  <td data-th="BRA-2014" id="BRA-2014">2014</td>
+  <td data-th="BRA-2015" id="BRA-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-PER">Perú</td>
+    <td data-th="PER-2013" id="PER-2013">2013</td>
+    <td data-th="PER-2014" id="PER-2014">2014</td>
+    <td data-th="PER-2015" id="PER-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-CHL">Chile</td>
+    <td data-th="CHL-2013" id="CHL-2013">2013</td>
+    <td data-th="CHL-2014" id="CHL-2014">2014</td>
+    <td data-th="CHL-2015" id="CHL-2015">2015</td>
+</tr>
+<tr>
+    <td data-th="name-MEX">México</td>
+    <td data-th="MEX-2013" id="MEX-2013">2013</td>
+    <td data-th="MEX-2014" id="MEX-2014">2014</td>
+    <td data-th="MEX-2015" id="MEX-2015">2015</td>
+</tr>
+</table>
     </article>
 `
 }
+
+// Trabalhar estrutura para selecao de países
+// btnSubmit.addEventListener("click", function(e){
+//   let inputProdutoNome = document.getElementById('product-name').value;
+//   let inputProdutoPreco = document.getElementById('product-price').value;
+//   let inputProdutoFoto = document.getElementById('product-img').value;
+
+//   let novoItem = {
+//     "product": {
+//       "name": "",
+//       "images":[],
+//       "price": {
+//         "value": 0,
+//       }
+//     }
+//   };
